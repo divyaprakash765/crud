e5fd962a22831afc0895a1570641f3106b156fb8
divya prakash <2022ugce092@nitjsr.ac.in>
2025-09-21 13:43:41 +0530

    feat: add user management endpoints and update user DTO for CRUD operations

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Delete, Param, Get, Patch } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
@@ -10,4 +11,24 @@ export class UserController {
   async createUser(@Body() data: CreateUserDto) {
     return this.userService.createUser(data);
   }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string) {
+    return this.userService.deleteUser(id);
+  }
+
+  @Get()
+    async getAllUsers() {
+        return this.userService.getAllUsers();
+    }
+
+    @Get(':id')
+    async getUserById(@Param('id') id: string) {
+        return this.userService.getUserById(id);
+    }
+
+    @Patch(':id')
+    async updateUser(@Param('id') id: string, @Body() data: UpdateUserDto) {
+        return this.userService.updateUser(id, data);
+    }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,6 +4,7 @@ import { DataSource } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserEntity } from 'src/db/entities/user-entity';
 import * as bcrypt from 'bcrypt';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
@@ -44,4 +45,58 @@ export class UserService {
       throw new NotFoundException(`${error.message}`);
     }
   }
+
+    //  update user
+    async updateUser(id: string, data: UpdateUserDto) {
+        try {
+            const user = await this.manager.findOneBy(UserEntity, {id});
+            if(!user) {
+                throw new Error('user not found');
+            }
+            user.name = data.name || user.name;
+            user.email = data.email || user.email;
+            await this.manager.save(user);
+            return { message: 'user updated successfully', data: {...data, id} };
+        } catch (error) {
+            throw new NotFoundException(`${error.message}`);
+        }
+    }
+
+    // delete user
+
+    async deleteUser(id: string) {
+        try {
+            const user = await this.manager.findOneBy(UserEntity, {id});
+            if(!user) {
+                throw new Error('user not found');
+            }
+            await this.manager.delete(UserEntity, {id});
+            return {message: 'user deleted successfully'};
+        } catch (error) {
+      throw new NotFoundException(`${error.message}`);
+        }
+    }
+
+    // get all user
+    async getAllUsers() {
+        try {
+            const users = await this.manager.find(UserEntity);
+            return users;
+        } catch (error) {
+            throw new NotFoundException(`${error.message}`);
+        }
+    }
+
+    // get user by id
+    async getUserById(id: string) {
+        try {
+            const user = await this.manager.findOneBy(UserEntity, {id});
+            if(!user) {
+                throw new Error('user not found');
+            }
+            return user;
+        } catch (error) {
+            throw new NotFoundException(`${error.message}`);
+        }
+    }
 }
